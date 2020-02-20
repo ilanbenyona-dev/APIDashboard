6c520514aca05c7d24003330a9670b21e9fc0f02
ilanbenyona-dev <ilnbenyona@gmail.com>
2020-02-20 19:44:29 +0200

    asdas

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # APIDashboard
+General project manager API made for a friend, written in Node.js.
--- models/user.js
+++
@@ -1,7 +0,0 @@
-const userSchema = new Schema({
-    is_admin: Boolean
-});
-
-
-const Project = mongoose.model('projects', userSchema);
-module.exports = Project;
